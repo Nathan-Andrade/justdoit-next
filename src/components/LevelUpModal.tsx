d36d08988fb65352bf097ad9a0df28571fb5ad28
Nathan Andrade <nathandulino2003@gmail.com>
2021-03-01 22:15:08 -0300

    settings

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -17,6 +17,7 @@ export function LevelUpModal(){
          <button type="button" onClick={closeLevelUpModal}>
            <img src="/icons/close.svg" alt="Fechar modal" />
          </button>
+         
        </div>
     </div>
    
